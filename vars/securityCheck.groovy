64ee97304704a014ee3f555b74f9028c9d132c8f
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-08 20:08:06 +0100

    Create securityCheck.groovy

---
+++ vars/securityCheck.groovy
@@ -0,0 +1,7 @@
+def call() {
+    stage('Security Check') {
+        echo 'Performing security checks...'
+        // Simulate security checks
+        bat 'echo "Performing security checks..."'
+    }
+}
