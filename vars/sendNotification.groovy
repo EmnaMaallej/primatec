57ab390f57feaea4bb7f2a039635f691ce0d6d1a
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-08 19:51:04 +0100

    Create sendNotification.groovy

---
+++ vars/sendNotification.groovy
@@ -0,0 +1,12 @@
+def call(String buildStatus) {
+    stage('Notification') {
+        def subject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${buildStatus}"
+        def body = "Check the build details at ${env.BUILD_URL}"
+
+        emailext (
+            subject: subject,
+            body: body,
+            recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+        )
+    }
+}
