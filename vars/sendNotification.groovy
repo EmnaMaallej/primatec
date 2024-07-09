a18e417a2ad88c100e248d23b3f48edb847cc85e
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-09 16:07:31 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,14 +1,9 @@
-def call(String buildStatus) {
-    stage('Notification') {
-        def subject = "${env.JOB_NAME} - Build #${env.BUILD_NUMBER} - ${buildStatus}"
-        def body = "Check the build details at ${env.BUILD_URL}"
-        
-        emailext (
-            subject: subject,
-            body: body,
-            recipientProviders: [[$class: 'DevelopersRecipientProvider']],
-            to: 'emna.maallej@gmail.com'
-        )
-    }
+def call(String result) {
+    echo "Sending notification for build result: ${result}"
+    emailext (
+        subject: "Jenkins Build: ${result}",
+        body: "The build has ${result}.",
+        to: 'emna.maallej@gmail.com'
+    )
 }
 
