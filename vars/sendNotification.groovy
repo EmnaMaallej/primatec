07013fb5d4f1fc1bdc173b965d0f8a7c94884f3f
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-09 16:31:58 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -3,7 +3,7 @@ def call(String result) {
     emailext (
         subject: "Jenkins Build: ${result}",
         body: "The build has ${result}.",
-        to: 'emna.maallej@gmail.com'
+        to: 'emna.maalej@supcom.tn'
     )
 }
 
