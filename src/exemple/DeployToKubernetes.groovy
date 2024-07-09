8c11e0f8650e3ee0d6f0bf5cff9da31f8c829bec
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-09 19:49:43 +0100

    Delete src/exemple/DeployToKubernetes.groovy

--- src/exemple/DeployToKubernetes.groovy
+++
@@ -1,7 +0,0 @@
-package example
-
-def call(Map params) {
-    def deploymentName = params.deploymentName
-    def image = params.image
-    sh "kubectl set image deployment/${deploymentName} ${deploymentName}=${image} --record"
-}
