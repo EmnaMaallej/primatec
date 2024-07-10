808c1a0e99c84c4e7ec070ebca88c3aa08309a02
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-10 11:54:40 +0100

    Update AgentChecker.groovy

--- src/com/exemple/AgentChecker.groovy
+++ src/com/exemple/AgentChecker.groovy
@@ -1,1 +1,27 @@
+// src/com/example/AgentChecker.groovy
+package com.example
+
+class AgentChecker implements Serializable {
+    def script
+
+    AgentChecker(script) {
+        this.script = script
+    }
+
+    boolean isUnix() {
+        return script.isUnix()
+    }
+
+    boolean hasLabel(String label) {
+        return script.node {
+            return script.env.NODE_LABELS?.contains(label)
+        }
+    }
+
+    String getOS() {
+        return script.node {
+            return script.isUnix() ? 'Unix' : 'Windows'
+        }
+    }
+}
 
