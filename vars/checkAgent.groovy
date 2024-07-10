164747e525ca117901506411cab1b194ef43a8cf
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-10 12:07:47 +0100

    Update checkAgent.groovy

--- vars/checkAgent.groovy
+++ vars/checkAgent.groovy
@@ -5,4 +5,11 @@ def call() {
     echo "Is Unix: ${agentChecker.isUnix()}"
     echo "Has Label 'docker': ${agentChecker.hasLabel('docker')}"
     echo "Operating System: ${agentChecker.getOS()}"
+    echo "Jenkins Version: ${agentChecker.getJenkinsVersion()}"
+    echo "Java Version: ${agentChecker.getJavaVersion()}"
+    echo "Free Disk Space: ${agentChecker.getFreeDiskSpace()} GB"
+    echo "Total Disk Space: ${agentChecker.getTotalDiskSpace()} GB"
+    echo "Environment Variable 'HOME': ${agentChecker.getEnvironmentVariable('HOME')}"
+    echo "Agent Name: ${agentChecker.getAgentName()}"
 }
+
