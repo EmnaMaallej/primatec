b271c4f1ec339e0126da1ea1124fd3254dac197f
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-10 12:07:25 +0100

    Update AgentChecker.groovy

--- src/com/example/AgentChecker.groovy
+++ src/com/example/AgentChecker.groovy
@@ -23,5 +23,44 @@ class AgentChecker implements Serializable {
             return script.isUnix() ? 'Unix' : 'Windows'
         }
     }
+
+    String getJenkinsVersion() {
+        return script.node {
+            return script.Jenkins.instance.version.toString()
+        }
+    }
+
+    String getJavaVersion() {
+        return script.node {
+            return System.getProperty("java.version")
+        }
+    }
+
+    long getFreeDiskSpace() {
+        return script.node {
+            def root = new File('/')
+            return root.getFreeSpace() / (1024 * 1024 * 1024) // in GB
+        }
+    }
+
+    long getTotalDiskSpace() {
+        return script.node {
+            def root = new File('/')
+            return root.getTotalSpace() / (1024 * 1024 * 1024) // in GB
+        }
+    }
+
+    String getEnvironmentVariable(String name) {
+        return script.node {
+            return script.env."${name}"
+        }
+    }
+
+    String getAgentName() {
+        return script.node {
+            return script.env.NODE_NAME
+        }
+    }
 }
+
 
