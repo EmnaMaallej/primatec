309f0999ec04e5b0484842b414ff5f652b6b0cd2
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-13 18:51:05 +0100

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -3,15 +3,8 @@ def call(String result) {
     emailext(
         subject: "Jenkins Build: ${result}",
         body: "The build has ${result}.",
-        to: 'emna.maallej@gmail.com',
-        from: 'your-email@gmail.com',
-        smtpServer: 'smtp.gmail.com',
-        replyTo: 'your-email@gmail.com',
-        mimeType: 'text/plain',
-        charset: 'UTF-8',
-        recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-        authUser: 'your-email@gmail.com',
-        authPassword: 'your-app-password'
+        to: 'emna.maallej@gmail.com'
     )
 }
+
 
