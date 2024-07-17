2db97faa71d8faefbd82becb92ed5b26db2db845
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-17 13:40:46 +0100

    Create node.groovy

---
+++ src/org/jenkins/node.groovy
@@ -0,0 +1,67 @@
+package org.jenkins
+
+import hudson.model.Computer
+import hudson.model.Node as JenkinsNode
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+
+import java.util.Date
+
+class Node {
+    private JenkinsNode node
+    private Computer computer
+
+    Node(JenkinsNode node) {
+        this.node = node
+        this.computer = node.toComputer()
+    }
+
+    String getName() {
+        return node.name
+    }
+
+    boolean isOnline() {
+        return computer ? !computer.isOffline() : false
+    }
+
+    boolean isTemporarilyOffline() {
+        return computer ? computer.isTemporarilyOffline() : false
+    }
+
+    boolean isIdle() {
+        return computer ? computer.isIdle() : false
+    }
+
+    int getNumberOfExecutors() {
+        return computer ? computer.countExecutors() : 0
+    }
+
+    String getExecutors() {
+        return computer ? computer.executors.collect { it.displayName }.join(', ') : "None"
+    }
+
+    Map getMonitorData() {
+        return computer ? computer.monitorData : [:]
+    }
+
+    Date getConnectTime() {
+        return computer ? new Date(computer.connectTime) : null
+    }
+
+    Date getLaunchTime() {
+        if (computer instanceof hudson.slaves.SlaveComputer) {
+            return new Date(computer.getConnectTime())
+        } else {
+            return null
+        }
+    }
+
+    String getOfflineCause() {
+        return computer ? (computer.offlineCause?.toString() ?: 'None') : 'None'
+    }
+
+    static List<Node> getAllNodes() {
+        def jenkins = Jenkins.instance
+        def nodes = jenkins.nodes
+        return nodes.collect { new Node(it) }
+    }
+}
