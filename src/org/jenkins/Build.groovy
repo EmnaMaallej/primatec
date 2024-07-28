3492f0ea7473c0b14b3a1ad486227e0978f86b82
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-28 13:46:07 +0100

    Update Build.groovy

--- src/org/jenkins/Build.groovy
+++ src/org/jenkins/Build.groovy
@@ -4,7 +4,7 @@ import hudson.model.AbstractProject
 import hudson.model.Run
 import hudson.model.Result
 
-class Build {
+class Build implements JenkinsEntity {
     private AbstractProject project
     private Run lastBuild
 
@@ -13,7 +13,8 @@ class Build {
         this.lastBuild = project.lastBuild
     }
 
-    String getProjectName() {
+    @Override
+    String getName() {
         return project.fullName
     }
 
@@ -25,10 +26,30 @@ class Build {
         return lastBuild ? (lastBuild.result ? lastBuild.result.toString() : 'IN_PROGRESS') : 'No builds found'
     }
 
+    JobInfo getJob() {
+        JobInfo.getAllJobs().find { it.getName() == this.getProjectName() }
+    }
+
+    Node getNode() {
+        def executor = this.lastBuild.getExecutor()
+        def owner = executor.getOwner()
+        owner.getNode()
+    }
+
     static List<Build> getAllBuilds() {
         def jenkins = Jenkins.instance
         def projects = jenkins.getAllItems(AbstractProject.class)
         return projects.collect { new Build(it) }
     }
+
+    static Build getBuildByNumber(String jobName, int buildNumber) {
+        def job = JobInfo.getAllJobs().find { it.getName() == jobName }
+        if (job) {
+            def builds = job.getBuilds()
+            return builds.find { it.getLastBuildNumber() == buildNumber }
+        }
+        return null
+    }
 }
+
 
