da1952adb62be8e9a893f73243348138d1b1788c
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-29 11:53:39 +0100

    Create Nodes.groovy

---
+++ src/org/jenkins/Nodes.groovy
@@ -0,0 +1,59 @@
+import jenkins.model.Jenkins
+
+class Nodes {
+    String name
+
+    Nodes(String name) {
+        this.name = name
+    }
+
+    def getNodeInstance() {
+        return Jenkins.instance.getNode(name)
+    }
+
+    def getName() {
+        return getNodeInstance().displayName
+    }
+
+    def isIdle() {
+        return getNodeInstance().isIdle()
+    }
+
+    def getNumExecutors() {
+        return getNodeInstance().numExecutors
+    }
+
+    def getMode() {
+        return getNodeInstance().mode.toString()
+    }
+
+    def getLabels() {
+        return getNodeInstance().assignedLabels
+    }
+
+    def getDescription() {
+        return getNodeInstance().nodeDescription
+    }
+
+    def getOfflineCause() {
+        return getNodeInstance().getOfflineCauseReason()
+    }
+
+    def getLaunchTimeout() {
+        return getNodeInstance().launchTimeoutMilliseconds
+    }
+
+    def getAllProperties() {
+        return [
+            name            : getName(),
+            idle            : isIdle(),
+            numExecutors    : getNumExecutors(),
+            mode            : getMode(),
+            labels          : getLabels(),
+            description     : getDescription(),
+            offlineCause    : getOfflineCause(),
+            launchTimeout   : getLaunchTimeout()
+        ]
+    }
+}
+
