d5d28332ba038e5300903888507e1c3ad95545e0
EmnaMaallej <emna.maalej@supcom.tn>
2024-07-29 11:54:52 +0100

    Create Jobs.groovy

---
+++ src/org/jenkins/Jobs.groovy
@@ -0,0 +1,60 @@
+import jenkins.model.Jenkins
+
+class Jobs {
+    String name
+
+    Jobs(String name) {
+        this.name = name
+    }
+
+    def getJobInstance() {
+        return Jenkins.instance.getJob(name)
+    }
+
+    def getJobName() {
+        return getJobInstance().displayName
+    }
+
+    def getDescription() {
+        return getJobInstance().description
+    }
+
+    def getLastBuild() {
+        return getJobInstance().lastBuild.number
+    }
+
+    def getLastCompletedBuild() {
+        return getJobInstance().lastCompletedBuild.number
+    }
+
+    def getNextBuildNumber() {
+        return getJobInstance().nextBuildNumber
+    }
+
+    def getBuilds() {
+        return getJobInstance().builds.collect { it.number }
+    }
+
+    def getBuildProperties(int buildNumber) {
+        def build = new Builds(buildNumber, this)
+        return build.getAllProperties()
+    }
+
+    def getAllBuildsAndNodeProperties() {
+        def builds = getBuilds()
+        return builds.collect { buildNumber ->
+            getBuildProperties(buildNumber)
+        }
+    }
+
+    def getAllProperties() {
+        return [
+            name                 : getJobName(),
+            description          : getDescription(),
+            lastBuild            : getLastBuild(),
+            lastCompletedBuild   : getLastCompletedBuild(),
+            nextBuildNumber      : getNextBuildNumber(),
+            builds               : getBuilds()
+        ]
+    }
+}
