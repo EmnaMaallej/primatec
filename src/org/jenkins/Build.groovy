d55bda497ed5b2f9c92caafad1f562807c704a03
EmnaMaallej <emna.maalej@supcom.tn>
2024-08-01 16:29:30 +0100

    Update Build.groovy

--- src/org/jenkins/Build.groovy
+++ src/org/jenkins/Build.groovy
@@ -5,10 +5,39 @@ import jenkins.model.Jenkins
 
 class Build {
     private Run build
+    private String jobName
+    private int buildNumber
 
     Build(String jobName, int buildNumber) {
         def job = Jenkins.instance.getItemByFullName(jobName)
         this.build = job ? job.getBuildByNumber(buildNumber) : null
+        this.jobName = jobName
+        this.buildNumber = buildNumber
+    }
+
+    Run getRun() {
+        def job = Jenkins.instance.getItemByFullName(jobName)
+        return job?.getBuildByNumber(buildNumber)
+    }
+
+    String getNodeName() {
+        def run = getRun()
+        if (run) {
+            def executor = run.getExecutor()
+            if (executor) {
+                def node = executor.getOwner().getNode()
+                if (node) {
+                    return node.getNodeName()
+                } else {
+                    println "Node not found for build ${buildNumber} of job ${jobName}"
+                }
+            } else {
+                println "Executor not found for build ${buildNumber} of job ${jobName}"
+            }
+        } else {
+            println "Run not found for build ${buildNumber} of job ${jobName}"
+        }
+        return "UNKNOWN"
     }
 
     int getNumber() {
