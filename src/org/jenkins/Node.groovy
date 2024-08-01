1ffe56e7d462f24e6dd1a7d17da228374b2c9a5b
EmnaMaallej <emna.maalej@supcom.tn>
2024-08-01 22:26:33 +0100

    Update Node.groovy

--- src/org/jenkins/Node.groovy
+++ src/org/jenkins/Node.groovy
@@ -15,15 +15,15 @@ class Node {
     }
 
     boolean isOnline() {
-        return node ? node.toComputer()?.isOnline() : false
+        return node?.toComputer()?.isOnline() ?: false
     }
 
     boolean isTemporarilyOffline() {
-        return node ? node.toComputer()?.isTemporarilyOffline() : false
+        return node?.toComputer()?.isTemporarilyOffline() ?: false
     }
 
     boolean isIdle() {
-        return node ? node.toComputer()?.isIdle() : false
+        return node?.toComputer()?.isIdle() ?: false
     }
 
     int getNumberOfExecutors() {
@@ -39,44 +39,45 @@ class Node {
     }
 
     Map<String, Object> getMonitorData() {
-        def monitor = node ? node.toComputer()?.monitor : null
+        def monitor = node?.toComputer()?.monitor
         return monitor ? [
-            "hudson.node_monitors.Jvm" : monitor.jvm,
-            "hudson.node_monitors.ResponseTime" : monitor.responseTime,
-            "hudson.node_monitors.SwapSpace" : monitor.swapSpace,
-            "hudson.node_monitors.TmpSpace" : monitor.tmpSpace,
-            "hudson.node_monitors.DiskSpace" : monitor.diskSpace,
+            "hudson.node_monitors.Jvm"        : monitor.jvm,
+            "hudson.node_monitors.ResponseTime": monitor.responseTime,
+            "hudson.node_monitors.SwapSpace"  : monitor.swapSpace,
+            "hudson.node_monitors.TmpSpace"   : monitor.tmpSpace,
+            "hudson.node_monitors.DiskSpace"  : monitor.diskSpace,
             "hudson.node_monitors.Throughput" : monitor.throughput
         ] : [:]
     }
 
     long getConnectTime() {
-        return node ? node.toComputer()?.connectTime : 0
+        return node?.toComputer()?.connectTime ?: 0
     }
 
     long getLaunchTime() {
-        return node ? node.toComputer()?.launchTime : 0
+        return node?.toComputer()?.launchTime ?: 0
     }
 
     String getOfflineCause() {
-        return node ? node.toComputer()?.offlineCause?.message : "Offline cause not available"
+        return node?.toComputer()?.offlineCause?.message ?: "Offline cause not available"
     }
 
     Map<String, Object> getNodeProperties() {
         return node ? [
-            "Name": getNodeName(),
-            "Online": isOnline(),
-            "Temporarily Offline": isTemporarilyOffline(),
-            "Idle": isIdle(),
-            "Number of Executors": getNumberOfExecutors(),
-            "Executors": getExecutors(),
-            "Monitor Data": getMonitorData(),
-            "Connect Time": getConnectTime(),
-            "Launch Time": getLaunchTime(),
-            "Offline Cause": getOfflineCause()
+            "Name"                : getNodeName(),
+            "Online"              : isOnline(),
+            "Temporarily Offline" : isTemporarilyOffline(),
+            "Idle"                : isIdle(),
+            "Number of Executors" : getNumberOfExecutors(),
+            "Executors"           : getExecutors(),
+            "Monitor Data"        : getMonitorData(),
+            "Connect Time"        : getConnectTime(),
+            "Launch Time"         : getLaunchTime(),
+            "Offline Cause"       : getOfflineCause()
         ] : [:]
     }
 }
+
 
 
 
